dc284fd327338f02a4ee9f668b5edf784301f29e
Laravel Daily <info@laraveldaily.com>
2023-06-07 20:12:44 +0000

    QuickAdminPanel automatic commit

--- resources/views/admin/teams/show.blade.php
+++ resources/views/admin/teams/show.blade.php
@@ -50,6 +50,22 @@
     </div>
 </div>
 
-
+<div class="card">
+    <div class="card-header">
+        {{ trans('global.relatedData') }}
+    </div>
+    <ul class="nav nav-tabs" role="tablist" id="relationship-tabs">
+        <li class="nav-item">
+            <a class="nav-link" href="#team_companies" role="tab" data-toggle="tab">
+                {{ trans('cruds.company.title') }}
+            </a>
+        </li>
+    </ul>
+    <div class="tab-content">
+        <div class="tab-pane" role="tabpanel" id="team_companies">
+            @includeIf('admin.teams.relationships.teamCompanies', ['companies' => $team->teamCompanies])
+        </div>
+    </div>
+</div>
 
 @endsection
