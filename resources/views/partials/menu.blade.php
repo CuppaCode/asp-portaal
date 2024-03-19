963602ce5c4da9312cc6b86d3169b0b964e2ee41
jwijnen <jeewee94@gmail.com>
2024-03-19 22:44:10 +0100

    Quickfix access for menuitem

--- resources/views/partials/menu.blade.php
+++ resources/views/partials/menu.blade.php
@@ -291,6 +291,7 @@
                 </a>
             </li>
         @endif
+        @can('mail_template_access')
         <li class="c-sidebar-nav-item">
             <a href="{{ route("admin.mail-templates.index") }}" class="c-sidebar-nav-link {{ request()->is("admin/mailTemplates") || request()->is("admin/mailTemplates/*") ? "c-active" : "" }}">
                 <i class="fa-fw fas fa-envelope c-sidebar-nav-icon">
@@ -299,6 +300,7 @@
                 {{ trans('cruds.mailTemplates.title') }}
             </a>
         </li>
+        @endcan
     </ul>
 
 </div>
