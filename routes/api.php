791797356d3ed51acc13c18581d7f0a7e70dd346
General Orange <erik.17021995@gmail.com>
2024-04-09 20:04:17 +0200

    API work

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,11 @@
 <?php
 
-Route::group(['prefix' => 'v1', 'as' => 'api.', 'namespace' => 'Api\V1\Admin', 'middleware' => ['auth:sanctum']], function () {
+Route::group([
+    'prefix' => 'v1',
+    'as' => 'api.',
+    'namespace' => 'Api\V1\Admin',
+    'middleware' => ['auth:sanctum']
+], function () {
     
     
 });
