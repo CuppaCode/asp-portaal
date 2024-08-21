171f3042587567f76c57a46698226bd9c65d165d
jwijnen <jeewee94@gmail.com>
2024-08-21 20:16:21 +0200

    Added sla code to controller

--- app/Http/Controllers/Admin/ClaimController.php
+++ app/Http/Controllers/Admin/ClaimController.php
@@ -21,6 +21,7 @@ use App\Models\Opposite;
 use App\Models\User;
 use App\Models\MailTemplate;
 use App\Models\Note;
+use App\Models\SLA;
 use Gate;
 use Illuminate\Http\Request;
 use Spatie\MediaLibrary\MediaCollections\Models\Media;
@@ -455,6 +456,9 @@ class ClaimController extends Controller
         $firstContact = Contact::where('company_id', $claim->company->id)->get()->first();
         $notesAndTasks = $claim->notes->merge($claim->tasks)->sortBy('created_at');
 
+        $sla = SLA::where('company_id', $claim->company->id)->first();
+        $users = User::where('team_id', $user->team->id)->get();
+
         $parentMediaArray = [
             trans('cruds.claim.fields.damage_files') => $claim->damage_files,
             trans('cruds.claim.fields.report_files') => $claim->report_files,
@@ -475,7 +479,7 @@ class ClaimController extends Controller
 
         $claim->load('company', 'injury_office', 'vehicle', 'vehicle_opposite', 'recovery_office', 'expertise_office', 'team', 'notes', 'tasks');
 
-        return view('admin.claims.show', compact('claim', 'firstContact', 'allContactsInCompany', 'opposite', 'users', 'notesAndTasks', 'mailTemplates', 'assignee_name', 'parentMediaArray'));
+        return view('admin.claims.show', compact('claim', 'firstContact', 'allContactsInCompany', 'opposite', 'users', 'notesAndTasks', 'mailTemplates', 'assignee_name', 'parentMediaArray', 'sla'));
     }
 
     public function destroy(Claim $claim)
