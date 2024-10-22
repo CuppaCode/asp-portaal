f3d743e079299a93392446504b18012d923dfd4d
Cuppacode | Erik <erik@cuppacode.nl>
2024-10-22 21:11:16 +0200

    Weird bug with mail templates fixed, due to view.blade.php getting that large....

--- resources/views/admin/claims/show.blade.php
+++ resources/views/admin/claims/show.blade.php
@@ -1055,7 +1055,10 @@
                                     {{ json_encode(App\Models\Claim::DAMAGED_AREA_SELECT) }}</div>
                                 <div class="d-none" id="damageOriginJson">
                                     {{ json_encode(App\Models\Claim::DAMAGE_ORIGIN) }}</div>
-
+                                <div class="d-none" id="damageKindJson">
+                                    {{ json_encode(App\Models\Claim::DAMAGE_KIND) }}</div>
+                                <div class="d-none" id="recoverableClaimJson">
+                                    {{ json_encode(App\Models\Claim::RECOVERABLE_CLAIM_SELECT) }}</div>
                             </div>
                             <div class="form-group">
                                 <button class="btn btn-danger" type="submit" name="add-task-dashboard" value='true'>
