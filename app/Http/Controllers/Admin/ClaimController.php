e3131df9448255b2b353c7541e174498fb072c13
Cuppacode | Erik <erik@cuppacode.nl>
2024-10-23 21:02:49 +0200

    Added declineClaim ajax function

--- app/Http/Controllers/Admin/ClaimController.php
+++ app/Http/Controllers/Admin/ClaimController.php
@@ -586,4 +586,22 @@ class ClaimController extends Controller
         return redirect()->back()->with('message', 'Mail is verstuurd en gelogd in dossier');
 
     }
+
+    public function declineClaim(Request $request)
+    {
+        $claim = Claim::find($request->claimID);
+
+        $claim->decline_reason = $request->declineReason;
+
+        $claim->status = 'claim_denied';
+
+        $claim->save();
+
+        return response()->json(
+            [
+                'type' => 'alert-danger',
+                'message' => 'Claim afgewezen door: ' . \App\Models\Claim::DECLINE_REASON_SELECT[$claim->decline_reason]
+            ], 200);
+
+    }
 }
