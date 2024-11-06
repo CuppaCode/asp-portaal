eedd7039869502d57c68964953938884069c5a2c
Erik van der Schaaf <43253748+GeneralOrange@users.noreply.github.com>
2024-11-06 20:46:06 +0100

    Development (#214)
    * New financial_access check on isAdmin
    
    * API work
    
    * API access tokens work
    
    * Testing routes
    
    * Create acces token function
    
    * Changed everything from API to AJAX
    
    * Fixed a typo in new access level
    
    * New permission edit
    
    * Bugfix for claim create
    
    * Switch isAdmin to canAssignCompany
    
    * master to dev (#183)
    
    * Order because customer requested prio on this
    
    * Added a few new variables for in the mail templates:
    [kenteken_wederpartij] [verhaalbaar] [schade_soort]
    [chauffeur_naam] [chauffeur_email]
    [wederpartij_naam] [wederpartij_adres] [wederpartij_postcode_stad] [wederpartij_telnr] [wederpartij_email]
    
    ---------
    
    Co-authored-by: jwijnen <jeewee94@gmail.com>
    Co-authored-by: Cuppacode | Erik <erik@cuppacode.nl>
    
    * Finaly fixed new agent role
    
    * Adjustment mail template (#188)
    
    * 164 add sla option (#182)
    
    * update 1, composer .... remove
    
    * Added SLA
    
    * Final SLA
    
    * Adjusted bg color
    
    ---------
    
    Co-authored-by: Erik van der Schaaf <43253748+GeneralOrange@users.noreply.github.com>
    
    * Revert "164 add sla option (#182)" (#190)
    
    This reverts commit ba79c647bef1b2008fc7b4206b248a6403cf1339.
    
    * Feature/mail send recipients attachments (#189)
    
    * Added attachments to mail from claim
    
    * Nice mailing functionality :-)
    
    ---------
    
    Co-authored-by: Cuppacode | Erik <erik@cuppacode.nl>
    
    * Fixed mailing template
    
    * 161 add notification if there is a new reply on a task or status change (#195)
    
    * Initial notification for tasks
    
    * Changed mailtemplate
    
    * Text adjustment
    
    * Claim id fix
    
    * Read more css adjustment (#196)
    
    * Sla ok (#192)
    
    * update 1, composer .... remove
    
    * Added SLA
    
    * Final SLA
    
    * Adjusted bg color
    
    * Added sla code to controller
    
    * Fix isadmin
    
    * Isadmin fix for claimcontroller
    
    ---------
    
    Co-authored-by: Erik van der Schaaf <43253748+GeneralOrange@users.noreply.github.com>
    
    * Quick fix for isAdmin and extra edit button.
    
    * Fixed read-more padding
    
    * Feature/claim denied dropdown (#199)
    
    * Init for new claim rejection modal
    
    * Lots of progress, adding migration and rest of UI and logic
    
    * Cleared out bug and added more clarification for when claim is declined.
    
    ---------
    
    Co-authored-by: Cuppacode | Erik <erik@cuppacode.nl>
    
    * Added partials to claims show blade
    
    * Weird bug with mail templates fixed, due to view.blade.php getting that large....
    
    * Final tweaks and adding final features
    
    * Added Claim DECLINE_REASON SELECT
    
    * Added declineClaim ajax function
    
    * Added launch Modal function
    
    * Update existing function with modal function
    
    * Updated quick update status
    
    * Last parts from declineClaim function added...
    
    * Added JSON variables for mail templates
    
    * Remove the 3
    
    * Fixed a bug with "Dylan", we got "Dylaned" (#213)
    
    Co-authored-by: Cuppacode | Erik <erik@cuppacode.nl>
    
    ---------
    
    Co-authored-by: Cuppacode | Erik <erik@cuppacode.nl>
    Co-authored-by: jwijnen <jeewee94@gmail.com>

---
+++ resources/views/partials/claims/show/overview.blade.php
@@ -0,0 +1,67 @@
+<div class="col-md-12">
+    <div class="card">
+        <div class="card-header d-flex justify-content-between align-items-center">
+            Schadedossier overzicht
+
+            @if ( isset($claim->decline_reason) && $claim->status == 'claim_denied')
+
+                <div class="alert alert-danger mb-0" role="alert">
+                    <strong>Reden afwijzing:</strong>
+                    {{ App\Models\Claim::DECLINE_REASON_SELECT[$claim->decline_reason] }}
+                </div>
+
+            @endif
+
+            @if( $claim->assign_self || $isAdminOrAgent)
+                <select class="form-control col-md-4" id="current-status" data-claim-id="{{ $claim->id }}">
+
+                    @foreach (App\Models\Claim::STATUS_SELECT as $key => $status)
+                        <option value="{{ $key }}" {{ $claim->status == $key ? 'selected' : '' }}>
+                            {{ $status }}</option>
+                    @endforeach
+
+                </select>
+            @else
+                <div class="col-md-3 btn btn-info">
+                    {{ App\Models\Claim::STATUS_SELECT[$claim->status] }}
+                </div>
+            @endif
+
+        </div>
+
+        <div class="card-body">
+            <div class="row">
+                <div class="col-md-2">
+                    <div class="card-title">
+                        {{ trans('cruds.claim.fields.claim_number') }}
+                    </div>
+                    {{ $claim->claim_number }}
+                </div>
+                <div class="col-md-3">
+                    <div class="card-title">
+                        {{ trans('cruds.claim.fields.company') }}</div>
+                    {{ $claim->company->name ?? '' }}
+                </div>
+                <div class="col-md-3">
+                    <div class="card-title">
+                        {{ trans('cruds.claim.fields.subject') }}</div>
+                    {{ $claim->subject }}
+                </div>
+                @if ($claim->opposite_claim_no)
+                    <div class="col-md-2">
+                        <div class="card-title">
+                            {{ trans('cruds.claim.fields.opposite_claim_no') }}</div>
+                        {{ $claim->opposite_claim_no }}
+                    </div>
+                @endif
+                @if ($assignee_name)
+                    <div class="col-md-2">
+                        <div class="card-title">
+                            {{ trans('cruds.claim.fields.assignee') }}</div>
+                        {{ $assignee_name->first_name ?? 'Verwijderde gebruiker' . ' ' . $assignee_name->last_name ?? '' }}
+                    </div>
+                @endif
+            </div>
+        </div>
+    </div>
+</div>
