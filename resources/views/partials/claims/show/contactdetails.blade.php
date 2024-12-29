8bcbedee273c747be0b517ebdf2edc2456d59e83
Erik van der Schaaf <43253748+GeneralOrange@users.noreply.github.com>
2024-12-29 22:25:26 +0100

    Some name changes as requested by client (#230)
    Co-authored-by: Cuppacode | Erik <erik@cuppacode.nl>

--- resources/views/partials/claims/show/contactdetails.blade.php
+++ resources/views/partials/claims/show/contactdetails.blade.php
@@ -1,7 +1,7 @@
 <div class="col-md-6">
     <div class="card">
         <div class="card-header d-flex justify-content-between align-items-center">
-            Contactgegevens
+            Contactgegevens klant
 
             @if( $claim->assign_self || $isAdminOrAgent)
                 <a class="btn btn-xs btn-success" href="{{ route('admin.claims.edit', $claim->id) }}">
@@ -20,7 +20,15 @@
                 <div class="card-title">
                     Email
                 </div>
-                <p class="card-text"><a href="mailto:{{ $firstContact->email }}">{{ $firstContact->email }} </a> </p>
+                <p class="card-text">
+                    <a href="mailto:{{ $firstContact->email }}">{{ $firstContact->email }} </a>
+                </p>   
+                <div class="card-title">
+                    Telefoonnummer
+                </div>
+                <p class="card-text">
+                    <a href="tel:{{ $claim->company->phone }}">{{ $claim->company->phone }} </a>
+                </p>
             @else
                 Nog geen contactpersoon bekend.
             @endisset
